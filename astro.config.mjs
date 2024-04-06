2a8c4c02720f35f92fb15b60f7d5e40df984c28b
Danny Burrows <dannyburrows@protonmail.com>
2024-04-06 14:45:49 +0100

    Blog base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,18 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danny-burrows.github.io',
-  integrations: [react(), sitemap()]
+  integrations: [react(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'monokai',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
 });
---
+++ src/pages/blog/post/test.md
@@ -0,0 +1,30 @@
+---
+layout: ../../../layouts/blog.astro
+title: "My First Blog Post!"
+author: "Danny Burrows"
+date: "19th Aug 2022"
+---
+
+This is just a testing document. Ive added some more text here to test what happens when there is a bunch of text that should wrap around.
+
+```c
+#include <stdio.h>
+
+int main(void) {
+
+    for (int i = 0; i < 10; i++)
+        printf("Hey\n");
+
+    return 0;
+}
+```
+
+```rust
+fn main() {
+    for i in 1..10 {
+        println!("Hey");
+    }
+}
+```
+
+Some conclusion to the blog post.
